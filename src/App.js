12781507fd99a477e6b91f9b21d45c64f34eae30
Josue Rodriguez <maybejosue@gmail.com>
2020-10-27 19:12:06 -0700

    • creates modal function to in order to call and display • creates local state in app js to keep track of modal open status • adds modal css styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {useState} from 'react'
+import {Modal} from './utils/Modal.js'
 
 function App() {
+const [open, setOpen] = useState(false)
+
+const toggle = () => {
+setOpen(!open)
+}
 
   return (
     <div className="App">
-      <p>Hello World</p>
+      <p>click</p>
+      {/* On click it calls a function that adds a class */}
+      <button onClick={toggle}>Modal</button>
+      {open ? (Modal()):(null)}
     </div>
   );
 }
